ad0320ea356811c6efe68f2dcf128a729c346047
iyeongjun <they6687@naver.com>
2021-08-17 15:00:27 +0900

    1. 헬스 체커추가 2. 이전 메세지 전부 가져오기 추가

--- app.js
+++ app.js
@@ -2,37 +2,64 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server,{path: "/talk/socket"});
+const {Server} = require("socket.io");
+const io = new Server(server, {path: "/talk/socket"});
 const port = 8084
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: '34.64.133.74',
+    user: 'pinstagram',
+    password: 'test1234',
+    database: 'pinstagram'
+});
 
-app.get('/talk/healthCheck',(req,res)=>{
-    console.log("Health Check OK")
+connection.connect()
+
+
+app.get('/talk/healthCheck', (req, res) => {
     res.json({
         message: "OK"
     })
 })
 
+app.get('/talk/getAllMessage', (req, res) => {
+    connection.query("SELECT * FROM message", (err, rows, fields) => {
+        res.json({
+            code: err ? "데이터베이스 오류" : "ok",
+            message: err ? "데이터베이스 오류" : null,
+            httpCode: err ? 500 : 200,
+            data: err ? null : rows
+        })
+    })
+})
+
 io.on('connection', client => {
-    client.on('hello',(data)=>{
+    client.on('hello', (data) => {
         console.log(`User Login ${data}`)
-        io.emit('hello',data)
+        io.emit('hello', data)
     })
 
-    client.on('say',(data)=>{
+    client.on('say', (data) => {
         console.log(`User Say ${data}`)
-        io.emit('say',data)
+        let {text,name,userId}  = JSON.parse(data)
+        connection.query(`insert into message (text, name, userId) values ('${text}','${name}',${userId})`)
+        io.emit('say', data)
     })
 
-    client.on('bye',(data)=>{
+    client.on('bye', (data) => {
         console.log(`User Logout ${data}`)
-        io.emit('bye',data)
+        io.emit('bye', data)
     })
     client.on('disconnect', (data) => {
-        console.log(`User Disconnected ${data}`)
+        console.log(`User Disconnected ${client} ${data}`)
+        client.disconnect()
     });
 });
 
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log("Server listen")
 });
+
+process.on('exit', () => {
+    connection.end()
+})
